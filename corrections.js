89b7ee84b1cd5bb4c21e5e4aeb192c872419af80
dm87 <danieljamesmcauley@gmail.com>
2016-02-26 05:43:35 +0000

    Corrections applied to Q2

---
+++ corrections.js
@@ -0,0 +1,59 @@
+// Render HTML with a receipt for an order
+function receipt(paytype) {
+    switch (paytype.payment_type){
+        case "creditcard":
+            p = "Payment info: " + order.payment.getCardType + " " + order.payment.card_number;
+            break;
+        case "paypal":
+            p = "Payment info: " + order.payment.paypal_info;
+            break;
+        case "manual":
+            p = "Payment info: " + order.payment.manual_payment_info;
+            break;
+        case "free":
+            p = "Payment info: This order was free!"
+            break;
+        default:
+            p = "Payment info: " + order.payment.default_payment_info;
+    }
+var ifNotFree = ''
+
+if (order.payment_type != "free"){
+    isNotFree = (<p>{receipt(order.payment_type)} was charged ${order.amount_in_dollars}</p>)
+}
+    
+html = function (){
+    return (
+        <div>
+        <h1> Order receipt details </h1>
+        <p>Your order of {order.products.name} has been received.</p> {ifNotFree}
+        </div>
+        )}
+        
+ document.write(html);    
+    
+
+
+
+////////ORIGINAL/////////
+// // Render HTML with a receipt for an order
+// function receipt(order) {
+//   if (order.payment_type = "creditcard") { // changed conditional equality to assignment operator
+//     p = "Payment info: " + order.payment.getCardType + " " + order.payment.card_number; //card type(VISA/MasterCard etc.) and number)
+//   } else if (order.payment_type == "paypal") {
+//     p = "Payment info: " + order.payment.paypal_info;
+//   } else if (order.payment_type == "manual") {
+//     p = "Payment info: " + order.payment.manual_payment_info;
+//   } else if (order.payment_type == "free") {
+//     p = "Payment info: This order was free!"; //Free order or promotional item
+//   } else if
+//   {
+//     p = "Payment info: " + order.payment.default_payment_info;//default order info
+//   }
+//   if (order.payment_type != "free") {
+//      p = p + "<p> was charged " + order.amount_in_dollars + "$" + "</p>";
+//   }
+ 
+//   html = "<p>" + "Your order of" + order.products.name + "has been received" + "</p>" + "<p>" + p + "</p>";
+//   document.write("<h1> Order receipt details </h1>" + html);
+// }
